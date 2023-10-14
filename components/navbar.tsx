30f8c4d1c32b101546c4ea081b84c9e62e57a3b7
dCOMMICS <sanwurld@gmail.com>
2023-10-14 20:10:43 +0300

    💪🏿🚀

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,7 +3,7 @@
 const Navbar = () => {
   return (
     
-    <nav className="border-2 border-red-500"></nav>  
+    <nav className="border-2 border-red-500 flexBetween max-container padding-container relative z-30 py-5"></nav>  
     
   )
 }
