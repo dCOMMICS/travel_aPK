a1851588f8e16ead5581fa0addab876f6c1289a8
dCOMMICS <sanwurld@gmail.com>
2023-10-14 16:41:52 +0300

    🔥💪🏿

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,12 @@
-import './globals.css'
-import type { Metadata } from 'next'
+import type { Metadata } from 'next';
 
+import './globals.css'
+import Navbar from '@/components/navbar';
+import Footer from '@/components/footer';
 
 export const metadata: Metadata = {
-  title: 'tRAVEL wEBSITE',
-  description: 'tRAVEL AND cAMPING WEBSITE',
+  title: 'Travel',
+  description: 'Travel UI/UX App for Camping',
 }
 
 export default function RootLayout({
@@ -15,9 +17,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-
-      {children}
-
+        <Navbar />
+        <main className="relative overflow-hidden">
+          {children}
+        </main>
+        <Footer />
       </body>
     </html>
   )
